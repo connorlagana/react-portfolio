4fd342c17c9b943cbde78bcea15a5ae07ca3bc5b
Connor Lagana <connor.lagana@gmail.com>
2020-01-26 17:20:33 -0500

    added pics

--- src/components/ContactPic.js
+++ src/components/ContactPic.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import githubLogo from "../images/ContactPics/github.png";
 
 class ContactPic extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pics: [
+        {
+          image: githubLogo,
+          title: "Github",
+          link: "https://github.com/connorlagana"
+        }
+      ]
+    };
+  }
   render() {
-    return <div>hello</div>;
+    return (
+      <div>
+        {this.state.pics.map((pic, key) => (
+          <div id="contactPic">
+            <a href={pic.link}>
+              <img src={pic.image} />
+            </a>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
