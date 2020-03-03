c2846b40e9f507258488735c96d71d3708f524cd
Connor Lagana <connor.lagana@gmail.com>
2020-03-03 11:09:14 -0500

    all href replaced

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import KeyboardBackspace from "@material-ui/icons/KeyboardBackspace";
 import IndTech from "./IndTech.js";
+import { Link } from "react-router-dom";
 
 class Technologies extends Component {
   render() {
     return (
       <div id="projectsPage">
         <div id="projectsTitle">
-          <a href="/" id="projectsTitle">
+          <Link to="/" id="projectsTitle">
             <KeyboardBackspace id="backArrow" />
             <h1 id="projectsH1">Technologies</h1>
-          </a>
+          </Link>
         </div>
         <div id="IndT">
           <IndTech />
