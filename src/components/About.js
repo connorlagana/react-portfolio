7509f51795129a9ce0cfa2ed6f88cfbc825d71fb
Connor Lagana <connor.lagana@gmail.com>
2020-03-03 14:57:33 -0500

    holy water

--- src/components/About.js
+++ src/components/About.js
@@ -17,29 +17,29 @@ class About extends Component {
             </Fade>
           </div>
 
-          <div id="aboutSecond">
+          <div className="aboutSecond">
             <Fade cascade left>
-              <p>
+              <div>
                 I'm a front end and mobile developer currently working for Odega
                 Studios. My role here allows me to work with various designs and
                 bring them to life with a creative process.
-              </p>
-              <p>
+              </div>
+              <div>
                 I started learning computer science in highschool with a group
                 of friends before getting my B.S. at Manhattan College. My time
                 at college allowed me to work on personal projects while learing
                 web and mobile development. After finishing my degree in 2019, I
                 jumped head first into the digital industry learning all I could
                 about the world of front end development.
-              </p>
-              <p>
+              </div>
+              <div>
                 Outside of work, I like to work on full stack web applications
-                using React for frontend and Ruby on Rails for a backend. This
+                using React for front end and Ruby on Rails for a backend. This
                 allows me play around with new technology and gives me a better
                 understanding of the digital industry as a whole. It's also
                 incredibly fun! If I'm not desiging or coding something I'll be
                 messing around with music production or analyzing stocks!
-              </p>
+              </div>
             </Fade>
           </div>
         </div>
